07652ae23a8f51b0f86dc73b320f7a1e7a3cb789
ken koontz <kenneth.koontz@gmail.com>
2015-03-03 12:28:28 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='benchmark-places',
-    version='0.0.1',
+    version='0.0.2',
     author=u'Ken Koontz',
     author_email='kenneth.koontz@gmail.com',
     packages=find_packages(),
