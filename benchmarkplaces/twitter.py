8b1c8140fa5a287b0a6359d176d54fa9907b5881
ken koontz <kenneth.koontz@gmail.com>
2015-07-27 16:37:21 -0700

    add serializer

--- benchmarkplaces/twitter.py
+++ benchmarkplaces/twitter.py
@@ -1,4 +1,6 @@
 import os
+import pytz
+from datetime import datetime
 
 from rauth import OAuth1Service, OAuth1Session
 
@@ -7,11 +9,28 @@ CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 BASE_URL = 'https://api.twitter.com/1.1/'
+FORMAT = '%a %b %d %H:%M:%S +0000 %Y' # twitter time format
 
 consumer_creds = {'consumer_key': CONSUMER_KEY,
                   'consumer_secret': CONSUMER_SECRET}
 access_token_creds = {'access_token': ACCESS_TOKEN,
                       'access_token_secret': ACCESS_TOKEN_SECRET}
+
+
+class TwitterSerializer(object):
+    def search_tweets(self, data):
+        serialized = []
+        for x in data:
+            tweet = {}
+            tweet['text'] = x.get('text')
+            tweet['review_id'] = x.get('id_str')
+            tweet['author'] = x.get('user', {}).get('screen_name')
+            tweet['source'] = 'twitter'
+            tweet['source_created_on'] = \
+                datetime.strptime(x.get('created_at'), FORMAT).replace(tzinfo=pytz.UTC)
+            tweet['oem'] = x
+            serialized.append(tweet)
+        return serialized
 
 
 class TwitterSession(OAuth1Session):
@@ -22,6 +41,8 @@ class TwitterSession(OAuth1Session):
 
 
 class Twitter(OAuth1Service):
+    serializer = TwitterSerializer()
+
     def __init__(self, *args, **kwargs):
         a = args if len(args) else [CONSUMER_KEY, CONSUMER_SECRET]
         kw = {'name': 'twitter',
@@ -36,7 +57,7 @@ class Twitter(OAuth1Service):
         response = self.get_session().get('search/tweets.json',
                                           params=params)
         if response.ok:
-            return response.json()['statuses']
+            return self.serializer.search_tweets(response.json()['statuses'])
         else:
             raise SourceError('An error occurred with %s API' % self.name,
                               response)
